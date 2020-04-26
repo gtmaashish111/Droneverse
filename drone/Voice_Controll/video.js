8c259d3c6f1a618d8e142de95d59e500f69eadb7
gtmaashish111 <50982000+gtmaashish111@users.noreply.github.com>
2020-04-26 15:39:35 +1000

    Removed files

--- drone/Voice_Controll/video.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const drone = require("dronestream");
-
-const server = http.createServer(function(req, res) {
-  require("fs").createReadStream(__dirname + "/public/video.html").pipe(res);
-});
-
-drone.listen(server);
-server.listen(4000);
